2c4646e98a78a4622a346e66887631d9b7f33724
Markus Mäkelä <markus.makela@mariadb.com>
2020-03-27 09:57:57 +0200

    MXS-2928: Add configurable sharedir
    This allows the GUI files to be stored in a safe, static place which isn't
    used to store data.

--- server/core/paths.cc
+++ server/core/paths.cc
@@ -26,6 +26,7 @@ static std::string config_persistdir = MXS_DEFAULT_CONFIG_PERSISTDIR;
 static std::string module_configdir = MXS_DEFAULT_MODULE_CONFIGDIR;
 static std::string logdir = MXS_DEFAULT_LOGDIR;
 static std::string libdir = MXS_DEFAULT_LIBDIR;
+static std::string sharedir = MXS_DEFAULT_SHAREDIR;
 static std::string cachedir = MXS_DEFAULT_CACHEDIR;
 static std::string datadir = MXS_DEFAULT_DATADIR;
 static std::string processdatadir = MXS_DEFAULT_DATADIR;
@@ -85,6 +86,11 @@ void set_libdir(const char* path)
     libdir = clean_up_pathname(path);
 }
 
+void set_sharedir(const char* path)
+{
+    sharedir = clean_up_pathname(path);
+}
+
 void set_execdir(const char* path)
 {
     execdir = clean_up_pathname(path);
@@ -98,6 +104,11 @@ void set_connector_plugindir(const char* path)
 const char* get_libdir()
 {
     return libdir.c_str();
+}
+
+const char* get_sharedir()
+{
+    return sharedir.c_str();
 }
 
 const char* get_cachedir()
